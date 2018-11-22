e2774d85d93420d1abfca8ed9bf161cbf7f62b80
rmcreyes <robinreyes1998@yahoo.com>
2018-11-21 17:47:32 -0800

    added spinner and options

---
+++ our_app/app/src/main/java/com/example/johnnyma/testbench/ProfessorActivity.java
@@ -0,0 +1,73 @@
+package com.example.johnnyma.testbench;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class ProfessorActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_professor);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.filter_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.question_sort, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id) {
+            case R.id.newest:
+                Toast.makeText(this, "Newest", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.oldest:
+                Toast.makeText(this, "Oldest", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.easiest:
+                Toast.makeText(this, "Easiest", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.hardest:
+                Toast.makeText(this, "Hardest", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.best_rated:
+                Toast.makeText(this, "Best rated", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.worst_rated:
+                Toast.makeText(this, "Worst rated", Toast.LENGTH_SHORT).show();
+                break;
+
+            default:
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
